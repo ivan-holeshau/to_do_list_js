7598a81dd59adb176b0a527b62d16dfc62bf5918
ivan-holeshau <goleshov85@gmail.com>
2020-05-17 21:06:19 +0300

    create oop code

--- script.js
+++ script.js
@@ -1,146 +1,143 @@
-let listCache = [];
+class Card {
+    _divCard;
 
-function createCard(id, text, finished) {
-  let clas = " my-card";
-  if (finished)
-    clas = " my-card-finished";
-  let divCard = document.createElement('div');
-  divCard.id = `div${id}`;
-  divCard.className = ` card ${clas} `;
+    constructor(id, text, finished) {
+        let clas = 'my-card';
+        if (finished)
+            clas = 'my-card-finished';
+        this._divCard = document.createElement('div');
+        this.divCard.id = `div${id}`;
+        this.divCard.className = ` card ${clas} `;
+        let checkBox = document.createElement('input');
+        checkBox.type = 'checkbox';
+        checkBox.id = `check${id}`;
+        checkBox.className = 'checkbox left';
+        if (finished)
+            checkBox.checked = 'true';
+        let labelChekbox = document.createElement('label');
+        labelChekbox.setAttribute("for", `check${id}`);
+        let iButtonDelete = document.createElement('i');
+        iButtonDelete.id = `i${id}`;
+        iButtonDelete.className = 'fa fa-trash-o right fa-2x';
+        let divText = document.createElement('div');
+        divText.innerHTML = text;
+        divText.className = 'center element-card';
+        this.divCard.append(checkBox);
+        this.divCard.append(labelChekbox);
+        let span = document.createElement('span');
+        span.className = 'spanvmiddle';
+        this.divCard.append(divText);
+        this.divCard.append(span);
+        this.divCard.append(iButtonDelete);
 
-  let checkBox = document.createElement('input');
-  checkBox.type = "checkbox";
-  checkBox.id = `check${id}`;
-  checkBox.className = 'checkbox left';
-  if (finished)
-    checkBox.checked = 'true';
-  let iButtonDelete = document.createElement('i');
-  iButtonDelete.id = `i${id}`;
-  iButtonDelete.className = 'fa fa-trash-o right fa-2x';
-
-  let divText = document.createElement('div');
-  divText.innerHTML = text;
-  divText.className = "center element-card";
-
-
-  divCard.append(checkBox);
-  let span = document.createElement('span');
-  span.className = 'spanvmiddle';
-
-  divCard.append(divText);
-  divCard.append(span);
-  divCard.append(iButtonDelete);
-
-  //divCard.append(divDelete);
-  return divCard;
+    }
+    get divCard() {
+        return this._divCard;
+    }
 }
 
+class TaskList {
+    _listTask = [];
 
-document.addEventListener('click', function (e) {
-  console.log(e.target.id);
-  if (e.target.id.indexOf('i') == 0)
-    deleteTask(e.target.id.slice(1));
-  if (e.target.id.indexOf('check') > -1)
-    finishedTask(e.target.id.slice(5));
-});
+    set listTask(list) {
+
+        if (list != null)
+            list.map(item => {
+                const {
+                    id,
+                    text,
+                    finished
+                } = item;
+                this.createTask(item);
+            });
+        else
+            this._listTask = [];
+    }
 
 
+    deleteTask(id) {
+        let li = document.getElementById(`${id}`);
+        let indexDeleteMass = this._listTask.findIndex(item => item.id == id);
+        this._listTask.splice(indexDeleteMass, 1);
+        li.remove();
+        this.saveTask();
+    }
 
-let countIdElement = 0;
+    saveTask() {
+        localStorage.setItem('list', JSON.stringify(this._listTask));
+    }
 
-function onLoad() {
+    finishedTask(id) {
+        let div = document.getElementById(`div${id}`);
+        let index = this._listTask.findIndex(item => item.id == id);
+        let s = this._listTask[index];
+        this._listTask[index].finished = !this._listTask[index].finished;
+        let clas = 'my-card';
+        if (document.getElementById(`check${id}`).checked) {
+            clas = 'my-card-finished';
+        }
+        div.className = `card ${clas}`;
+        this.saveTask();
+    }
 
-  listCache = JSON.parse(localStorage.getItem('list'));
-  if (listCache != null)
-    listCache.map(item => {
-      const {
+
+    createTask({
         id,
         text,
         finished
-      } = item;
-      addTaskNew(item);
-    });
-  else
-    listCache = [];
+    }) {
 
-
-}
-
-function addTaskNew({
-  id,
-  text,
-  finished
-}) {
-
-
-  let chek = false;
-  if (!id) {
-    id = ++countIdElement;
-    text = document.getElementById('task').value;
-    document.getElementById('task').value = '';
-    chek = true;
-    finished = false;
-  }
-
-  let checkTextOfNull = false;
-  if (text.trim().length > 0) {
-    checkTextOfNull = true;
-    //alert(checkTextOfNull)
-  }
-
-  if (checkTextOfNull) {
-    let li = document.createElement('li');
-
-    li.style = ` 
-        list-style-type: none; 
-       `;
-    li.id = id;
-
-    list = {
-      id: id,
-      text: text,
-      finished: finished
+        let chek = false;
+        let checkTextOfNull = false;
+        if (text.trim().length > 0) {
+            checkTextOfNull = true;
+        }
+        if (checkTextOfNull) {
+            let li = document.createElement('li');
+            li.style = ` 
+            list-style-type: none; 
+           `;
+            li.id = id;
+            let list = {
+                id: id,
+                text: text,
+                finished: finished
+            }
+            li.append(new Card(id, text, finished).divCard);
+            document.getElementById('list').append(li);
+            this._listTask.push(list);
+            localStorage.setItem('list', JSON.stringify(this._listTask));
+        }
     }
 
-    // li.innerHTML = createCard(id, text, finished);
-    li.append(createCard(id, text, finished));
-    document.getElementById('list').append(li);
-
-    if (chek) {
-      listCache.push(list);
-      localStorage.setItem('list', JSON.stringify(listCache));
-
-    } else {
-      countIdElement = ++id;
+    checkTaskOfNull(text) {
+        if (text.trim().length > 0)
+            return true;
+        else return false;
     }
-  }
 
+    createNewTask() {
+        this.createTask({
+            id: this._listTask.length,
+            text: document.getElementById('task').value,
+            finished: false,
+        });
+
+        document.getElementById('task').value = '';
+
+    }
 }
 
+document.addEventListener('click', function(e) {
+    if (e.target.id.indexOf('i') == 0)
+        taskList.deleteTask(e.target.id.slice(1));
+    if (e.target.id.indexOf('check') > -1)
+        taskList.finishedTask(e.target.id.slice(5));
+    if (e.target.className == 'btn-primary')
+        taskList.createNewTask();
+});
 
-function deleteTask(id) {
-  let li = document.getElementById(`${id}`);
-  let indexDeleteMass = listCache.findIndex(item => item.id == id);
-  listCache.splice(indexDeleteMass, 1);
-  li.remove();
-  localStorage.setItem('list', JSON.stringify(listCache));
-}
-
-function finishedTask(id) {
-  let div = document.getElementById(`div${id}`);
-
-
-  let index = listCache.findIndex(item => item.id == id);
-  let s = listCache[index];
-  listCache[index].finished = !listCache[index].finished;
-  let clas = 'my-card';
-
-  if (document.getElementById(`check${id}`).checked) {
-    clas = 'my-card-finished';
-  }
-
-  div.className = `card ${clas}`;
-
-  localStorage.setItem('list', JSON.stringify(listCache));
-}
-
+const taskList = new TaskList();
+window.onload = function() {
+    taskList.listTask = JSON.parse(localStorage.getItem('list'));
+};
